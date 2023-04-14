2dcdb8b4960ef74a41d544e7cfdc7fe6165d4218
Quitch <Quitch@users.noreply.github.com>
2023-04-14 15:09:12 +0100

    Determine game type

--- ui/mods/com.pa.quitch.ai-chat/live_game/communication.js
+++ ui/mods/com.pa.quitch.ai-chat/live_game/communication.js
@@ -5,6 +5,16 @@ if (!aiCommunicationsLoaded) {
 
   function aiCommunications() {
     try {
+      var determineGameType = function () {
+        // not a typo, PA Inc can't spell
+        if (model.gameOptions.isGalaticWar()) {
+          return "GW";
+        } else if (model.playerInTeam()) {
+          return "Teams";
+        }
+        return "FFA";
+      };
+
       var aiAlliesCount = _.filter(model.players(), {
         ai: 1,
         stateToPlayer: "ally",
@@ -12,6 +22,7 @@ if (!aiCommunicationsLoaded) {
       var startingPlanetCount = _.filter(model.planetListState().planets, {
         starting_planet: true,
       }).length;
+      var gameType = determineGameType();
     } catch (e) {
       console.error(e);
       console.error(JSON.stringify(e));
