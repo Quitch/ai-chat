c3a5c806f5d16e572a6f8167ef1541d959d6e43c
Quitch <Quitch@users.noreply.github.com>
2023-04-15 14:26:34 +0100

    Add TODO for spawning planets

--- ui/mods/com.pa.quitch.ai-chat/live_game/communication.js
+++ ui/mods/com.pa.quitch.ai-chat/live_game/communication.js
@@ -79,6 +79,7 @@ if (!aiCommunicationsLoaded) {
       model.players.subscribe(function () {
         ais = _.filter(model.players(), { ai: 1 });
         aiAllies = _.filter(ais, { stateToPlayer: "allied_eco" });
+        //TODO - we need to detect planets which spawn later
         planets = model.planetListState().planets.length - 1; // last entry in array isn't a planet
         startingPlanets = _.filter(model.planetListState().planets, {
           starting_planet: true,
